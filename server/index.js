149903ede1cb4747b8e3f5e5be3645904cccd2d6
Greg Vargas <greg.vargas@silvercar.com>
2019-08-20 16:37:33 -0500

    Added crud endpoints for boards

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const app = express();
 app.use(json());
 app.use(cors());
 
+// Boards
 app.get('/api/boards', boards.getBoards);
+app.post('/api/boards/:id', boards.addBoard);
+app.put('/api/boards/:id', boards.editBoard);
+app.delete('/api/boards/:id', boards.deleteBoard);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
